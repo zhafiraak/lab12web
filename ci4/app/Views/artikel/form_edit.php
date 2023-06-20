958449fba35dcda54c046dab5a580c91c881e8cd
zhafiraak <zhafirakhafianti19@gmail.com>
2023-06-20 12:15:06 +0700

    tambah file

---
+++ ci4/app/Views/artikel/form_edit.php
@@ -0,0 +1,19 @@
+<?= $this->include('template/admin_header'); ?>
+<h2><?= $title; ?></h2>
+<form action="" method="post" enctype="multipart/form-data">
+ <p>
+ <input type="text" name="judul" value="<?= $data['judul'];?>" >
+ </p>
+ <p>
+ <textarea name="isi" cols="50" rows="10"><?=
+$data['isi'];?></textarea>
+ </p>
+ <p>
+<input type="file" name="gambar">
+</p>
+ <p>
+    <br>
+    <input type="submit" value="Kirim" class="btn btn-large">
+</p>
+</form>
+<?= $this->include('template/admin_footer'); ?>
